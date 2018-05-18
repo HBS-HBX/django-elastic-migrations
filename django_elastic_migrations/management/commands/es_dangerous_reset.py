6733a86bbe71c06fb122cd2020d82bf907d9cad7
Myer Nore <pnore@hbs.edu>
2018-05-18 13:17:54 -0400

    add message to ./manage.py es_dangerous_reset
    PD-14588 elasticsearch common indexing and upgrade command - part 2

--- django_elastic_migrations/management/commands/es_dangerous_reset.py
+++ django_elastic_migrations/management/commands/es_dangerous_reset.py
@@ -1,12 +1,17 @@
+import logging
 from django.core.management import call_command
 
 from django_elastic_migrations.management.commands.es import ESCommand
 
+logger = logging.getLogger("django-elastic-migrations")
 
 class Command(ESCommand):
     help = "django-elastic-migrations: dangerously reset all indexes"
 
     def handle(self, *args, **options):
+        msg = "Dangerously resetting all Elasticsearch indexes in ./manage.py es_dangerous_reset!"
+        logger.warning(msg)
+        print msg
         call_command('es_drop', all=True, force=True)
         call_command('es_create', all=True)
         call_command('es_activate', all=True)
