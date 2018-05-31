cc716708535fd4de153dbd093488d1f8ee5c7097
Myer Nore <pnore@hbs.edu>
2018-05-31 16:47:01 -0400

    move common user search methods into GenericDEMDocType
    * add resume_mode to ./manage.py es_update
    * update releases.py
    
    PD-14627 Elasticsearch User Index Real Time Updates And Cohort Search

--- django_elastic_migrations/management/commands/es_update.py
+++ django_elastic_migrations/management/commands/es_update.py
@@ -9,20 +9,29 @@ class Command(ESCommand):
 
     def add_arguments(self, parser):
         self.get_index_specifying_arguments(parser, include_newer=True)
+        parser.add_argument(
+            '--resume', action='store_true',
+            help=("Only update documents that have changed since "
+                  "the last ./manage.py es_update. NOTE: DEMDocType subclass "
+                  "needs to implement this.")
+        )
 
     def handle(self, *args, **options):
         indexes, exact_mode, apply_all, _, newer_mode = self.get_index_specifying_options(options)
+        resume_mode = options.get('resume', False)
 
         if apply_all:
             DEMIndexManager.update_index(
                 'all',
                 exact_mode=exact_mode,
-                newer_mode=newer_mode
+                newer_mode=newer_mode,
+                resume_mode=resume_mode
             )
         elif indexes:
             for index_name in indexes:
                 DEMIndexManager.update_index(
                     index_name,
                     exact_mode=exact_mode,
-                    newer_mode=newer_mode
+                    newer_mode=newer_mode,
+                    resume_mode=resume_mode
                 )
