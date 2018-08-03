b26f87b664855e460107949b6b5e820cf88ea4ca
Myer Nore <pnore@hbs.edu>
2018-08-03 16:54:25 -0400

    add `.encode('utf-8')` to the `json_str`
    myer/add python 3 tests #5

--- django_elastic_migrations/utils/es_utils.py
+++ django_elastic_migrations/utils/es_utils.py
@@ -5,8 +5,7 @@ import json
 
 def get_index_hash_and_json(index):
     spec = index.to_dict()
-    # note - need to add .encode('utf-8') for py3
-    json_str = json.dumps(spec, sort_keys=True)
+    json_str = json.dumps(spec, sort_keys=True).encode('utf-8')
     md5_hash = hashlib.md5()
     md5_hash.update(json_str)
     return md5_hash.hexdigest(), json_str
