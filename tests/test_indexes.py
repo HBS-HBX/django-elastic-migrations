4b30859ec10da7f78dbe3eca2bf160e521b841ab
Myer Nore <pnore@hbs.edu>
2018-08-03 13:08:45 -0400

    add test to ensure that multiple versions of DEM work with DEMIndex.save
    #9 type error DEMIndex save

--- tests/test_indexes.py
+++ tests/test_indexes.py
@@ -6,8 +6,12 @@ Tests for the `django-elastic-migrations` models module.
 
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
+from elasticsearch import TransportError
+from elasticsearch_dsl.exceptions import IllegalOperation
+
 from django_elastic_migrations import DEMIndexManager
 from django_elastic_migrations.utils.test_utils import DEMTestCase
+from tests.es_config import ES_CLIENT
 from tests.search import MovieSearchIndex, MovieSearchDoc
 from tests.models import Movie
 
@@ -34,3 +38,15 @@ class TestMovie(DEMTestCase):
         results = MovieSearchDoc.get_search(movie_title).execute()
         first_result = results[0]
         self.assertEqual(str(movie.id), first_result.meta.id)
+
+    def test_save_index_elasticsearch_dsl_lt_62(self):
+        """
+        Added for #9 type error DEMIndex save
+        Tests that DEMIndex.save(using=using) works in 6.1 and 6.2 versions of DEM
+        """
+        try:
+            # if elasticsearch_dsl <= 6.2, using is not a parameter but shouldn't throw exception
+            MovieSearchIndex.save(using=ES_CLIENT)
+        except (TransportError, IllegalOperation):
+            # the indexes already exist
+            pass
