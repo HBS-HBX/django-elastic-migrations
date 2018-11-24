2f1944f1a7c56a7ac73c045712b05f7f3168387c
Myer Nore <pnore@hbs.edu>
2018-11-24 08:04:19 -0500

    remove python 2 compatibility from OmdbAPIQuery and Movie test objects
    #30 multiprocessing tests

--- tests/models.py
+++ tests/models.py
@@ -2,10 +2,8 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Movie(models.Model):
     title = models.CharField(max_length=500, unique=True)
     year = models.IntegerField()
