1e782828416e0a839ffd25b9942b52634766aaa8
Divya Dusi <ddusi@hbs.edu>
2025-07-21 14:42:29 -0400

    Fix elasticsearch-dsl compatibility: Remove deprecated super().doc_type() calls
    - Replace super().doc_type() calls with direct doc_type handling
    - Fixes AttributeError: 'super' object has no attribute 'doc_type'
    - Compatible with newer elasticsearch-dsl versions that removed doc_type method
    - Maintains backward compatibility by using stored __doc_type attribute

--- django_elastic_migrations/indexes.py
+++ django_elastic_migrations/indexes.py
@@ -885,7 +885,8 @@ class DEMIndex(ESIndex):
             if active_version_name and self.__doc_type._doc_type:
                 self.__doc_type._doc_type.index = active_version_name
 
-            return super(DEMIndex, self).doc_type(doc_type)
+            # Return the doc_type directly since newer elasticsearch-dsl versions don't have doc_type method
+            return self.__doc_type
         else:
             if self.get_version_id() and not self.__doc_type:
                 version_model = self.get_version_model()
@@ -893,7 +894,8 @@ class DEMIndex(ESIndex):
                 doc_type = self.__base_dem_index.doc_type()
                 doc_type_index_backup = doc_type._doc_type.index
                 doc_type._doc_type.index = version_model.name
-                self.__doc_type = super(DEMIndex, self).doc_type(doc_type)
+                # Use doc_type directly since newer elasticsearch-dsl versions don't have doc_type method
+                self.__doc_type = doc_type
                 if not self.hash_matches(version_model.json_md5):
                     doc_type._doc_type.index = doc_type_index_backup
                     our_hash, our_json = self.get_index_hash_and_json()
